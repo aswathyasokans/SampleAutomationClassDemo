876fadc2d741730ebea06acda3b6018f7a5e36f0
Aswathy Asokan S <aswathyasokans99@gmail.com>
2023-09-17 11:49:44 +0530

    commit...

---
+++ SampleAutomationClassDemo/src/main/java/debugdemo/AdditionDebug.java
@@ -0,0 +1,22 @@
+package debugdemo;
+
+public class AdditionDebug {
+	public static int add(int a,int b)
+	{
+		int sum=a+b;
+		return sum;
+	}
+
+	
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		int a=5,b=3;
+		int result=add(a,b);
+		System.out.println(result);
+		
+		
+
+	}
+
+}
---
+++ SampleAutomationClassDemo/src/main/java/debugdemo/Subdemo.java
@@ -0,0 +1,21 @@
+package debugdemo;
+
+public class Subdemo {
+	public static int sub(int a,int b)
+	{
+		int s=a-b;
+		return s;
+	}
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5,b=3;
+		int result=sub(a,b);
+		System.out.println(result);
+		
+
+	}
+
+}
